8f582e821a1fc379e0724ea62d9e4d7a5f09cc5b
Sofia Morales <sofimoralesroa92@gmail.com>
2025-04-15 19:13:16 +0200

    se agrega services/config

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,19 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import service from "../services/config.js";
 
 export default function Footer() {
+  useEffect(() => {
+    service
+      .get(`/recipes`)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <footer className="bottom-[0.2cm]  px-6 py-4 flex flex-col items-center">
       <h1 className="text-3xl font-bold tracking-tight text-[#47307D]">
